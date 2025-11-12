45ff3b79840444b4408537d46c86b853d303055b
ahmedkhedr <ahmedkhedrx1@gmail.com>
2025-11-12 11:13:40 +0200

    Remove localization files and dependencies
    - Deleted generated localization files: messages_all.dart, messages_ar.dart, messages_en.dart, l10n.dart, intl_ar.arb, intl_en.arb.
    - Removed localization-related code from main.dart.
    - Cleaned up pubspec.yaml by removing unused dependencies related to Firebase, localization, and other packages.
    - Updated pubspec.lock to reflect the removal of these dependencies.

--- lib/Constant.dart
+++
@@ -1,1 +0,0 @@
-const KisOnBoardingViewSeen='isOnBoardingViewSeen';
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:fruit_hub/Core/services/Getit__Services.dart';
-import 'package:fruit_hub/Core/services/shared_preferences_singelton.dart';
-import 'package:fruit_hub/Core/utils/App_Router.dart';
-import 'package:fruit_hub/Core/utils/App_colors.dart';
-import 'package:fruit_hub/firebase_options.dart';
-import 'package:fruit_hub/generated/l10n.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await Prefs.init();
-  setupGetIt();
   runApp(const FruitHub());
 }
 
@@ -24,20 +10,8 @@ class FruitHub extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-          fontFamily: 'Cairo',
-          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor)),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      locale: Locale('ar'),
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return MaterialApp(
+      home: Scaffold(),
     );
   }
 }
